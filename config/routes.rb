62e549d3e4a68111e760b6f21d32213c15e199f2
kurnia muhamad <omenkzz@gmail.com>
2017-04-22 15:23:07 +0700

    API versioning

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
-  resources :products
-  resources :users
-  resources :product_images
-  resources :product_prices
-  resources :product_categories
+
+  constraints subdomain: 'api' do
+    scope module: 'api' do
+      namespace :v1 do
+        resources :products
+        resources :users
+        resources :product_images
+        resources :product_prices
+        resources :product_categories
+      end
+    end
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
